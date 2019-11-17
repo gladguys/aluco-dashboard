ef02f570537432827c6a71bcbf7a69d52abbe390
denis <dineresc@gmail.com>
2019-11-17 19:15:48 -0300

    get values from form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +35,8 @@ import { ClassesComponent } from './classes/classes.component';
     AppRoutingModule,
     CustomMaterialModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '../models/student.model';
 import { StudentService } from '../services/student.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: 'students.component.html',
     styleUrls: ['students.component.scss']
 })
-export class StudentsComponent implements OnInit {   
+export class StudentsComponent implements OnInit {
 
     students: Student[] = [];
+    studentForm: FormGroup;
 
-    constructor(private studentService: StudentService){}
+    constructor(private studentService: StudentService, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.studentService.getList()
-            .subscribe((res:Student[]) => {
+            .subscribe((res: Student[]) => {
                 this.students = res;
             })
+
+        this.studentForm = this.formBuilder.group({
+            registrationNumber: ['', Validators.required],
+            name: ['', Validators.required],
+            email: [''],
+            gender: [''],
+            dateBirth: [''],
+            phone: [''],
+            responsableName: [''],
+            address: ['']
+        });
+    }
+
+    save() {
+        const newStudent = this.studentForm.getRawValue();
+        console.log(newStudent); 
     }
 }
