e430617a96533aa29690f216e95860367f3114dd
denis <dineresc@gmail.com>
2019-11-18 00:00:46 -0300

    creating var file

--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Class } from '../models/class.model';
 
-const API_URL = "http://localhost:8080/api";
-
 @Injectable({ providedIn: 'root' })
 export class ClassService {
     
