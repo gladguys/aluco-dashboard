38d8b09c30e53fae40400e98278d535a6c074db2
denis <dineresc@gmail.com>
2019-11-17 23:24:01 -0300

    add auto to list classes

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -7,31 +7,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
     templateUrl: 'classes.component.html',
     styleUrls: ['classes.component.scss']
 })
-export class ClassesComponent implements OnInit {   
+export class ClassesComponent implements OnInit {
 
     classes: Class[] = [];
     classForm: FormGroup;
 
-    constructor(private classService:ClassService, private formBuilder: FormBuilder){}
+    constructor(private classService: ClassService, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
         this.classService.getList()
-            .subscribe((res:Class[]) => {
+            .subscribe((res: Class[]) => {
                 this.classes = res;
             });
-        
+
         this.classForm = this.formBuilder.group({
             name: ['', Validators.required],
             description: ['', Validators.required],
 
         });
     }
-    
+
     save() {
         const newClass = this.classForm.getRawValue();
-        console.log(newClass); 
+        console.log(newClass);
         this.classService.save(newClass).subscribe(res => {
-            
+            this.classForm.reset();
+            this.classService.getList()
+                .subscribe((res: Class[]) => {
+                    this.classes = res;
+                });
         });
     }
 }
