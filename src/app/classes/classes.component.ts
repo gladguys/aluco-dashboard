c87b938743b1e0e0756afeb7717fcc1422492913
denis <dineresc@gmail.com>
2019-11-17 23:11:23 -0300

    post for classess

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClassService } from '../services/class.service';
 import { Class } from '../models/class.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: 'classes.component.html',
@@ -9,13 +10,28 @@ import { Class } from '../models/class.model';
 export class ClassesComponent implements OnInit {   
 
     classes: Class[] = [];
+    classForm: FormGroup;
 
-    constructor(private classService:ClassService){}
+    constructor(private classService:ClassService, private formBuilder: FormBuilder){}
 
     ngOnInit() {
         this.classService.getList()
             .subscribe((res:Class[]) => {
                 this.classes = res;
-            })
+            });
+        
+        this.classForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            description: ['', Validators.required],
+
+        });
+    }
+    
+    save() {
+        const newClass = this.classForm.getRawValue();
+        console.log(newClass); 
+        this.classService.save(newClass).subscribe(res => {
+            
+        });
     }
 }
--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Class } from '../models/class.model';
 
 const API_URL = "http://localhost:8080/api";
 
 @Injectable({ providedIn: 'root' })
 export class ClassService {
-
     
     constructor(private http: HttpClient) {}
 
+    save(newClass: Class) {
+        return this.http.post(API_URL + "/classes", newClass);
+    }
 
     getList() {
         return this.http.get(API_URL + "/classes");
